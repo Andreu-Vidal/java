df44bdb568a3c52ddc6e9e2832613fe9d49b5d7d
Andreu Vidal <andreuvidal@outlook.com>
2021-02-23 19:57:00 +0100

    Nuevos ejercicios

---
+++ condiciones/Condición_if.java
@@ -0,0 +1,76 @@
+package condiciones;
+
+public class Condición_if {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/* Sólo es un comando
+		 * if (condición) comando1;
+		 * 
+		 * if (condición)
+		 * {
+		 * 
+		 * 	s1;
+		 * 	s2;
+		 *	s3;
+		 * 
+		 * }
+		 * 
+		 * if (condición)
+		 * {
+		 * 
+		 * 	s1;
+		 * 	s2;
+		 * 	s3;
+		 * 
+		 * }
+		 * else
+		 * {
+		 * 	s1;
+		 * 	s2;
+		 * 
+		 * }
+		 * 
+		 * if (condición)
+		 * {
+		 * 
+		 * 	s1;
+		 * 	s2;
+		 * 	s3;
+		 * 
+		 * }
+		 * else if (condición){
+		 * 	s1;
+		 * 	s2;
+		 * 	}
+		 * else{
+		 * 	s1;
+		 * 	s2;
+		 * 
+		 * }
+		 * 
+		 */
+		
+		int num1=10;
+		
+		if (num1%2==0)
+		{
+			System.out.println("Par");
+		}
+		else
+		{
+			System.out.println("Impar");
+		}
+	
+		//Pedir una cadena por teclado
+		
+		System.out.println("Por favor, introduce tu nombre: ");
+				
+
+		
+		
+		
+	}
+
+}
---
+++ variables/ClausulaSwith.java
@@ -0,0 +1,33 @@
+package variables;
+
+public class ClausulaSwith {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=1;
+		
+		switch(x)
+		{
+		case 0: System.out.println("cero");
+			break;
+		
+		case 1: System.out.println("uno");
+			break;
+		
+		case 2: System.out.println("dos");
+			break;
+			
+		default:
+			System.out.println("ninguna");
+				
+		
+		
+		
+		}
+		
+		
+		
+	}
+
+}
